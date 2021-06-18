f0dd0dc7c9718815e887ffdfd528e793b1ed6272
Jessica Calkins <jessicacalkins795213@gmail.com>
2021-06-18 14:27:48 -0500

    first commit

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, request,redirect, session
+from flask.templating import render_template
+app = Flask(__name__)
+app.secret_key = 'root'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    print("Got Post Info")
+    session['username'] = request.form['name']
+    session['useremail'] = request.form['email']
+    return redirect("/show")
+
+@app.route('/show')
+def show_user():
+    # print("Showing the user info from the form")
+    # print(request.form)
+    # return render_template("show.html", name_on_template = session['username'], email_on_template = session['useremail'])
+
+    #another way of doing this
+    return render_template('show.html')
+    # check show.html to see the rest
+
+if __name__ == '__main__':
+    app.run(debug = True)
